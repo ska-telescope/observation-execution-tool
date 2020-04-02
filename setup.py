6c8381335689d4e6404a95e6ca2add93b66dcdfb
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2020-04-02 14:25:43 +0100

    bumped to version 0.4.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='observation-execution-tool',
-    version="0.4.0",
+    version="0.4.1",
     description="",
     long_description=readme + '\n\n',
     author="Your Name",
