a25bdfc0d4bf57430139b9587f6e031205029863
Viivi Pursiainen <viivi.pursiainen@stfc.ac.uk>
2020-09-15 16:25:23 +0100

    AT2-520 Add mock imports

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -21,6 +21,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+autodoc_mock_imports = ['tango', 'astropy', 'marshmallow']
+
 
 def setup(app):
     app.add_stylesheet('css/custom.css')
