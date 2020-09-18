0f02628c9c928b41aa8e14a55e303b317909e43f
Viivi Pursiainen <viivi.pursiainen@stfc.ac.uk>
2020-09-18 12:42:33 +0100

    Add empty line between docstring description and parameters

--- oet/observingtasks.py
+++ oet/observingtasks.py
@@ -754,6 +754,7 @@ def end(subarray: domain.SubArray):
     """
     Send the 'end' command to the SubArrayNode, marking the end of the
     current observation.
+
     :param subarray: the subarray to command
     :return:
     """
@@ -785,6 +786,7 @@ def abort(subarray: domain.SubArray):
     """
     Send the 'abort' command to the SubArrayNode, halt the subarray
     activity.
+
     :param subarray: the subarray to command
     :return:
     """
@@ -817,6 +819,7 @@ def obsreset(subarray: domain.SubArray):
     """
     Send the 'ObsReset' command to the SubArrayNode, which resets
     the SubArrayNode state to IDLE.
+
     :param subarray: the subarray to command
     :return:
     """
@@ -848,6 +851,7 @@ def restart(subarray: domain.SubArray):
     """
     Send the 'restart' command to the SubArrayNode which sets
     the SubArrayNode from ABORTED or FAULT state to EMPTY.
+
     :param subarray: the subarray to command
     :return:
     """
