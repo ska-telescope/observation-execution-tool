7c9437ff980b79463dbbb0433a25bd373a72a219
Liz <liz.bartlett@stfc.ac.uk>
2020-09-22 13:46:29 +0100

    better test to see if the config file is found

--- tests/oet/test_features.py
+++ tests/oet/test_features.py
@@ -5,6 +5,7 @@ Unit tests for the features module
 from configparser import ConfigParser
 from oet.features import Features
 import oet
+import os.path
 
 
 def test_pubsub_feature_returns_true_when_enabled_in_file():
@@ -87,8 +88,10 @@ def test_can_read_config_file(tmpdir):
 
 def test_default_config_file_is_read_if_present():
     """
-    Test to make sure that the default config file is read if it is present
-    Added to catch a bug found during code review
+    Test to make sure that the default config file is found
+    Added to catch a bug found during code review.
     """
 
-    assert oet.FEATURES.use_polling_to_read_tango_attributes is False
+    file = os.path.isfile(os.path.join(os.path.split(oet.ROOT_DIR)[0], 'oet.ini'))
+
+    assert file is True
