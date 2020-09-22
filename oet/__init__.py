72793f2938a75f740a8ae94419402ffcb99af5ef
Liz <liz.bartlett@stfc.ac.uk>
2020-09-22 11:44:29 +0100

    fixed some lint errors

--- oet/__init__.py
+++ oet/__init__.py
@@ -4,8 +4,5 @@ from .features import Features
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-#print(ROOT_DIR)
-#print(os.path.join(os.path.split(ROOT_DIR)[0],'oet.ini'))
-
 FEATURES = Features.create_from_config_files(os.path.expanduser('~/oet.ini'),
-                                             os.path.join(os.path.split(ROOT_DIR)[0],'oet.ini'))
+                                             os.path.join(os.path.split(ROOT_DIR)[0], 'oet.ini'))
