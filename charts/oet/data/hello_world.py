52c1d11d2429d07ecc5135d0f1cf2b35d87d9fb7
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2020-11-10 14:03:12 +0000

    Remove oet-scripts dependency from hello_world script

--- charts/oet/data/hello_world.py
+++ charts/oet/data/hello_world.py
@@ -1,6 +1,8 @@
+import functools
 import logging
 import os
 import time
+
 
 LOG = logging.getLogger(__name__)
 FORMAT = '%(asctime)-15s %(message)s'
@@ -14,19 +16,15 @@ def init(subarray_id: int):
     LOG.info(f'Script bound to sub-array {subarray_id}')
 
 
-def announce(msg):
-    publish_event_message(msg=msg)
-
-
 def _main(subarray_id: int, raise_msg=None):
-    announce(f'Running script in OS process {os.getpid()}')
+    LOG.info(f'Running script in OS process {os.getpid()}')
 
-    for i in range(10):
-        announce(f'pretending to execute scan {i}/10')
+    for i in range(5):
+        LOG.info(f'pretending to execute scan {i}/10')
         time.sleep(1)
 
     if raise_msg:
-        announce(f'Raising an exception with msg {raise_msg}')
+        LOG.error(f'Raising an exception with msg {raise_msg}')
         raise Exception(raise_msg)
 
-    announce('Script complete')
+    LOG.info('Script complete')
