58d0d22fb92a72d952b54b3ad97b2747e8ecbf0d
Stewart Williams <16945415+bravostuzero@users.noreply.github.com>
2020-11-16 21:56:34 +0000

    bumped to version 2.8.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as readme_file:
 
 setuptools.setup(
     name='observation-execution-tool',
-    version="2.8.5",
+    version="2.8.6",
     description="This project contains the code for the Observation Execution Tool, the application which provides high-level scripting facilities and a high-level scripting UI for the SKA.",
     long_description=readme + '\n\n',
     author="Stewart Williams",
