58c235224b2ce9ea0f8b7422150469c69ce05f78
Stewart Williams <3161869-bravostuzero@users.noreply.gitlab.com>
2022-05-13 15:52:43 +0100

    AT2-1322 Promote common test strings to module variables

--- tests/unit/ska_oso_oet/procedure/test_environmentmanager.py
+++ tests/unit/ska_oso_oet/procedure/test_environmentmanager.py
@@ -10,6 +10,9 @@ import pytest
 from ska_oso_oet.procedure.environment import Environment, EnvironmentManager
 from ska_oso_oet.procedure.gitmanager import GitArgs
 
+COMMIT_HASH = "69e93d57916f837ee93ca125f2785f0f6e21974d"
+VENV_SITE_PKGS = "/venv/lib/python3.7/site-packages"
+
 
 @pytest.fixture(scope="module")
 def env_manager():
@@ -30,17 +33,17 @@ def env_object(env_manager):
     when test run is complete.
     """
     env = Environment(
-        env_id="69e93d57916f837ee93ca125f2785f0f6e21974d",
+        env_id=COMMIT_HASH,
         created=multiprocessing.Event(),
         creating=multiprocessing.Event(),
-        location=f"{env_manager.base_dir}69e93d57916f837ee93ca125f2785f0f6e21974d",
-        site_packages=f"{env_manager.base_dir}69e93d57916f837ee93ca125f2785f0f6e21974d/venv/lib/python3.7/site-packages",
+        location=os.path.join(env_manager.base_dir, COMMIT_HASH),
+        site_packages=os.path.join(env_manager.base_dir, COMMIT_HASH, VENV_SITE_PKGS),
     )
     return env
 
 
 def test_environment_is_returned_when_hash_exists(env_manager, env_object):
-    commit = "69e93d57916f837ee93ca125f2785f0f6e21974d"
+    commit = COMMIT_HASH
     env_manager._envs = {commit: env_object}  # pylint: disable=protected-access
 
     result = env_manager.create_env(GitArgs(git_commit=commit))
@@ -56,14 +59,14 @@ def test_environment_is_returned_when_hash_exists(env_manager, env_object):
 def test_environment_is_created_when_hash_is_new(
     mock_subprocess_fn, mock_venv_fn, mock_git_fn, env_manager
 ):
-    commit = "69e93d57916f837ee93ca125f2785f0f6e21974d"
+    commit = COMMIT_HASH
     mock_git_fn.return_value = commit
     mock_venv_fn.side_effect = None
     mock_subprocess_fn.return_value = subprocess.CompletedProcess(
         args="",
         returncode=1,
         stdout=bytes(
-            f"{env_manager.base_dir}{commit}/venv/lib/python3.7/site-packages",
+            os.path.join(env_manager.base_dir, COMMIT_HASH, VENV_SITE_PKGS),
             "utf-8",
         ),
     )
@@ -72,28 +75,23 @@ def test_environment_is_created_when_hash_is_new(
 
     assert 1 == len(env_manager._envs)  # pylint: disable=protected-access
     assert result.env_id == commit
-    assert (
-        result.site_packages
-        == f"{env_manager.base_dir}{commit}/venv/lib/python3.7/site-packages"
+    assert result.site_packages == os.path.join(
+        env_manager.base_dir, commit, VENV_SITE_PKGS
     )
     del env_manager._envs[commit]  # pylint: disable=protected-access
 
 
 def test_delete_env(env_manager, env_object):
-    env_manager._envs = {  # pylint: disable=protected-access
-        "69e93d57916f837ee93ca125f2785f0f6e21974d": env_object
-    }
+    env_manager._envs = {COMMIT_HASH: env_object}  # pylint: disable=protected-access
     venv.create(
-        env_dir=f"{env_manager.base_dir}69e93d57916f837ee93ca125f2785f0f6e21974d",
+        env_dir=os.path.join(env_manager.base_dir, COMMIT_HASH),
         clear=True,
         system_site_packages=False,
         with_pip=False,
         symlinks=False,
     )
 
-    env_manager.delete_env("69e93d57916f837ee93ca125f2785f0f6e21974d")
+    env_manager.delete_env(COMMIT_HASH)
 
     assert 0 == len(env_manager._envs)  # pylint: disable=protected-access
-    assert not os.path.exists(
-        f"{env_manager.base_dir}69e93d57916f837ee93ca125f2785f0f6e21974d/"
-    )
+    assert not os.path.exists(os.path.join(env_manager.base_dir, COMMIT_HASH))
