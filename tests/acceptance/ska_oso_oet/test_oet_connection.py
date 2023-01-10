753eeefd2538b43b257ad68c390ad2f5420c8c0f
Viivi <viivi.pursiainen@stfc.ac.uk>
2023-01-10 12:32:12 +0000

    BTN-1630 Try fix oet connection acceptance test

--- tests/acceptance/ska_oso_oet/test_oet_connection.py
+++ tests/acceptance/ska_oso_oet/test_oet_connection.py
@@ -32,9 +32,7 @@ def execution_ends_in_expected_state(sbd_id):
     # This doesn't quite test that the OET can connect to postgres. Rather, it tests the test runner can connect.
     # This test was just added in BTN-1622 to show the chart is included properly, once the OET functionality is
     # expanded to include a call to the ODA, this test can be improved.
-    uow = RESTUnitOfWork(
-        f"http://ska-db-oda-rest-test:5000/{KUBE_NAMESPACE}/api/v1/sbds"
-    )
+    uow = RESTUnitOfWork(f"http://ska-db-oda-rest-test:5000/{KUBE_NAMESPACE}/api/v1")
     with pytest.raises(KeyError):
         with uow:
             uow.sbds.get(sbd_id)
