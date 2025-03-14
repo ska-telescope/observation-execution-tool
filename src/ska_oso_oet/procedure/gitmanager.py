756aeeab2ee6a4f6c58ae010c4b58739a276ee40
Liz Bartlett <liz.bartlett@stfc.ac.uk>
2025-03-14 11:37:26 +0000

    BTN-2642 adding override __init__ method to handle positional arguments with pydantic BaseModel

--- src/ska_oso_oet/procedure/gitmanager.py
+++ src/ska_oso_oet/procedure/gitmanager.py
@@ -2,7 +2,7 @@
 Static helper functions for cloning and working with a Git repository
 """
 import os
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 from typing import Optional
 from urllib.parse import urlparse
 
@@ -21,7 +21,14 @@ class GitArgs(BaseModel):
     git_branch: Optional[str] = None
     git_commit: Optional[str] = None
 
-    def __post_init__(self):
+    def __init__(self,
+                 git_repo: str = "https://gitlab.com/ska-telescope/oso/ska-oso-scripting.git",
+                 git_branch: str = None,
+                 git_commit: str = None):
+        super(GitArgs,self).__init__(git_repo=git_repo, git_branch=git_branch, git_commit=git_commit)
+
+    @model_validator(mode="after")
+    def post_init(self):
         # We only want to set the default branch if the commit isn't set, as the user
         # might just give a commit hash from a feature branch
         if self.git_branch is None and self.git_commit is None:
